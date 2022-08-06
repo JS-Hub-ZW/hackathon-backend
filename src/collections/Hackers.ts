9c9c95a41613ccf9f3b894aa6ba6b536a6bfe4ec
Takunda Madechangu <madechangu.takunda@gmail.com>
2022-08-06 22:19:45 +0200

    Remove fields

--- src/collections/Hackers.ts
+++ src/collections/Hackers.ts
@@ -8,53 +8,52 @@ const Hackers: CollectionConfig = {
   },
   fields: [
     {
-        name: 'image',
-        type: 'text'
+      name: 'image',
+      type: 'text'
     },
     {
-        name: 'gitlab',
-        type: 'text'
+      name: 'gitlab',
+      type: 'text'
     },
     {
-        name: 'github',
-        type: 'text'
+      name: 'github',
+      type: 'text'
     },
     {
-        name: 'linkedin',
-        type: 'text'
+      name: 'linkedin',
+      type: 'text'
     },
     {
-        name: 'twitter',
-        type: 'text'
+      name: 'twitter',
+      type: 'text'
     },
     {
-        name: 'website',
-        type: 'text'
+      name: 'website',
+      type: 'text'
     },
     {
-        name: 'subscribed',
-        type: 'checkbox',
-        defaultValue: false,
+      name: 'subscribed',
+      type: 'checkbox',
+      defaultValue: false,
     },
-
     {
-        name: 'banned',
-        type: 'select',
-        options: [
-          {
-            value: 'yes',
-            label: 'Yes',
-          },
-          {
-            value: 'no',
-            label: 'No',
-          },
-        ],
-        defaultValue: 'no',
-        admin: {
-          position: 'sidebar',
-        }
-      },
+      name: 'banned',
+      type: 'select',
+      options: [
+        {
+          value: 'yes',
+          label: 'Yes',
+        },
+        {
+          value: 'no',
+          label: 'No',
+        },
+      ],
+      defaultValue: 'no',
+      admin: {
+        position: 'sidebar',
+      }
+    },
 
     // Relationships
     {
