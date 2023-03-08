6254c8e638312f5793879966dcc68d2803ae137f
Takunda Madechangu <takundam@sonity.net>
2023-03-08 13:30:04 +0200

    Added user schema

---
+++ src/schemas/users.js
@@ -0,0 +1,15 @@
+// Define schema
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema({
+  name: String,
+  surname: String,
+  email: String,
+  password: String,
+  isAdmin: Boolean,
+  created_at: Date,
+  updated_at: { type: Date, default: Date.now() }
+});
+
+// Compile model from schema
+const UserModel = mongoose.model("userSchema", userSchema);
