5803a434f01ff806f930cba5d7f18b486b96baac
Takunda Madechangu <takundam@sonity.net>
2023-03-26 15:14:17 +0200

    Add events route to the app

--- src/app.js
+++ src/app.js
@@ -7,6 +7,8 @@ require('dotenv').config()
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var eventsRouter = require('./routes/events');
+
 
 var app = express();
 
@@ -19,6 +21,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/events', eventsRouter)
 
 
 const connectToDB = async () => {
