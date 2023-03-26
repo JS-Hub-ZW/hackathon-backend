34068b7dbc0fd6338a3f026dccd24339e7df9eaf
Takunda Madechangu <takundam@sonity.net>
2023-03-26 15:00:31 +0200

    Added mock data for events

---
+++ data/events.js
@@ -0,0 +1,109 @@
+events = [{
+    "date": "2023-04-15",
+    "time": "10:00am",
+    "name": "Spring Fair",
+    "venue": "Central Park",
+    "hosts": [
+      {
+        "name": "John Smith",
+        "email": "john.smith@example.com"
+      },
+      {
+        "name": "Jane Doe",
+        "email": "jane.doe@example.com"
+      }
+    ],
+    "sponsors": [
+      {
+        "name": "ABC Company",
+        "website": "https://www.abc.com/"
+      },
+      {
+        "name": "XYZ Inc.",
+        "website": "https://www.xyz.com/"
+      }
+    ],
+    "contacts": [
+      {
+        "name": "Joe Brown",
+        "email": "joe.brown@example.com",
+        "phone": "+1-555-1234"
+      },
+      {
+        "name": "Sara Jones",
+        "email": "sara.jones@example.com",
+        "phone": "+1-555-5678"
+      }
+    ],
+    "imagePortrait": "https://www.example.com/images/spring-fair-portrait.jpg",
+    "imageLandscape": "https://www.example.com/images/spring-fair-landscape.jpg",
+    "admission": "Free",
+    "type": "Community event",
+    "prizes": [
+        {
+            id: 1,
+            amount: 300, 
+            name: "1st Prize",
+            description: "Some descripiton here"
+        },
+        {
+            id: 2,
+            amount: 200, 
+            name: "2nd Prize",
+            description: "Some descripiton here"
+        },
+        {
+            id: 3,
+            amount: 100, 
+            name: "2rd Prize",
+            description: "Some descripiton here"
+        }
+    ]
+  },
+
+  {
+    "date": "2023-04-15",
+    "time": "10:00am",
+    "name": "Spring Fair",
+    "venue": "Central Park",
+    "hosts": [
+      {
+        "name": "John Smith",
+        "email": "john.smith@example.com"
+      },
+      {
+        "name": "Jane Doe",
+        "email": "jane.doe@example.com"
+      }
+    ],
+    "sponsors": [
+      {
+        "name": "ABC Company",
+        "website": "https://www.abc.com/"
+      },
+      {
+        "name": "XYZ Inc.",
+        "website": "https://www.xyz.com/"
+      }
+    ],
+    "contacts": [
+      {
+        "name": "Joe Brown",
+        "email": "joe.brown@example.com",
+        "phone": "+1-555-1234"
+      },
+      {
+        "name": "Sara Jones",
+        "email": "sara.jones@example.com",
+        "phone": "+1-555-5678"
+      }
+    ],
+    "imagePortrait": "https://www.example.com/images/spring-fair-portrait.jpg",
+    "imageLandscape": "https://www.example.com/images/spring-fair-landscape.jpg",
+    "admission": "$5",
+    "type": "Community event",
+    "prizes": []
+  }]
+
+
+  export default events 
