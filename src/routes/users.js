717bdda5bdf9c14ce5c3ff71e8e77d376a9643c8
Takunda Madechangu <takundam@sonity.net>
2023-04-02 18:06:45 +0200

    Add user endpoints

--- src/routes/users.js
+++ src/routes/users.js
@@ -3,9 +3,105 @@ const User = require('../schemas/users');
 var router = express.Router();
 
 /* GET users listing. */
-router.get('/', async function(req, res, next) {
-  const users = await User.find({})
-  res.send(users);
+router.get('/:id', async function(req, res, next) {
+  
+  try{
+    let userId = req.params.id
+
+    if (userId){
+      let user = await User.findById(userId)
+      res.send({
+        status: true,
+        message: "Operation was successful",
+        data: user
+      });
+    }else{
+      let users = await User.find({})
+      res.send({
+        status: true,
+        message: "Operation was successful",
+        data: users
+      });
+    }
+
+  }catch(error){
+    res.send({
+      status: false,
+      message: "Operation was not successful",
+      data: error
+    });
+  }
 });
+
+
+/* POST users create. */
+router.post('/create', async function(req, res, next) {
+  try {
+    let user = req.body
+
+    const newUser = new User(user)
+    await newUser.save()
+
+    res.send({
+      status: true,
+      message: "Operation was successful",
+      data: newUser
+    });
+
+  }catch(error){
+    res.send({
+      status: false,
+      message: "Operation was not successful",
+      data: error
+    })
+  }
+});
+
+/* PUT users update. */
+router.put('/update/:id', async function(req, res, next) {
+  try {
+    let user = req.body
+    let id = req.params.id
+
+    const updatedUser = await User.findByIdAndUpdate(id, user, {new: true})
+
+    res.send({
+      status: true,
+      message: "Operation was successful",
+      data: updatedUser
+    });
+
+  }catch(error){
+    res.send({
+      status: false,
+      message: "Operation was not successful",
+      data: error
+    })
+  }
+});
+
+/* DELETE users delete. */
+router.delete('/delete/:id', async function(req, res, next) {
+  try {
+    let id = req.params.id
+
+    const deletedUser = await User.findByIdAndDelete(id)
+
+    res.send({
+      status: true,
+      message: "Operation was successful",
+      data: deletedUser
+    });
+
+  }catch(error){
+    res.send({
+      status: false,
+      message: "Operation was not successful",
+      data: error
+    })
+  }
+});
+
+
 
 module.exports = router;
