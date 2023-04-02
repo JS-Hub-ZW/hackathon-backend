95cfc6e83243c54938503d1e73225e89abb0caa0
Takunda Madechangu <takundam@sonity.net>
2023-04-02 18:01:22 +0200

    Add try catches to catch errors

--- src/routes/events.js
+++ src/routes/events.js
@@ -6,29 +6,38 @@ var router = express.Router();
 /* GET events listing. */
 router.get('/:type?', async function(req, res, next) {
 
-  let type = req.params.type
-  let results = []
+  try {
+    let type = req.params.type
+    let results = []
 
 
 
-  if (type){
-    results = await Event.find({type: type})
-  }else{
-    results = await Event.find({})
+    if (type){
+      results = await Event.find({type: type})
+    }else{
+      results = await Event.find({})
+    }
+
+
+    res.send({
+      status: true,
+      message: "Operation was successful",
+      data: results
+    }); 
+  } catch (error) {
+    res.send({
+      status: false,
+      message: "Operation was not successful",
+      data: error
+    });
   }
-
-
-  res.send({
-    status: true,
-    message: "Operation was successful",
-    data: results
-  });
 });
 
 
 /* POST events create. */
 router.post('/create', function(req, res, next) {
 
+   try {
     let event = req.body
 
     const newEvent = new Event(event)
@@ -40,29 +49,46 @@ router.post('/create', function(req, res, next) {
       message: "Operation was successful",
       data: newEvent
     });
+
+   }catch(error){
+    res.send({
+      status: false,
+      message: "Operation was not successful",
+      data: error
+    })
+   }
   });
 
 
 /* POST events update. */
 router.post('/update/:id', async function(req, res, next) {
 
-    let eventId = req.params.id
-    let eventData = req.body
+    try{
+      let eventId = req.params.id
+      let eventData = req.body
 
-    const updatedEvent = await Event.findByIdAndUpdate(eventId, eventData, { new: true });
+      const updatedEvent = await Event.findByIdAndUpdate(eventId, eventData, { new: true });
+      
     
-  
-    res.send({
-      status: true,
-      message: "Operation was successful",
-      data: updatedEvent
-    });
+      res.send({
+        status: true,
+        message: "Operation was successful",
+        data: updatedEvent
+      });
+    }catch(error){
+      res.send({
+        status: false,
+        message: "Operation was not successful",
+        data: error
+      })
+    }
   });
 
   
   /* POST events delete. */
 router.post('/delete/:id', async function(req, res, next) {
 
+   try{
     let eventId = req.params.id 
 
     const deletedEvent = await Event.findByIdAndDelete(eventId)
@@ -73,6 +99,13 @@ router.post('/delete/:id', async function(req, res, next) {
       message: "Operation was successful",
       data: deletedEvent
     });
+   }catch(error){
+    res.send({
+      status: false,
+      message: "Operation was not successful",
+      data: error
+    })
+   }
   });
   
 
