0347dba2772340cbc3497d7e85c3722281cc6c51
Takunda Madechangu <takundam@sonity.net>
2023-07-03 18:40:00 +0200

    Add events

--- data/events.js
+++ data/events.js
@@ -1,163 +1,273 @@
 events = [{
-    "date": "2023-04-15",
-    "time": "10:00am",
-    "name": "Spring Fair",
-    "venue": "Central Park",
-    "hosts": [
-      {
-        "name": "John Smith",
-        "email": "john.smith@example.com"
-      },
-      {
-        "name": "Jane Doe",
-        "email": "jane.doe@example.com"
-      }
-    ],
-    "sponsors": [
-      {
-        "name": "ABC Company",
-        "website": "https://www.abc.com/"
-      },
-      {
-        "name": "XYZ Inc.",
-        "website": "https://www.xyz.com/"
-      }
-    ],
-    "contacts": [
-      {
-        "name": "Joe Brown",
-        "email": "joe.brown@example.com",
-        "phone": "+1-555-1234"
-      },
-      {
-        "name": "Sara Jones",
-        "email": "sara.jones@example.com",
-        "phone": "+1-555-5678"
-      }
-    ],
-    "imagePortrait": "https://www.example.com/images/spring-fair-portrait.jpg",
-    "imageLandscape": "https://www.example.com/images/spring-fair-landscape.jpg",
-    "admission": "Free",
-    "type": "community_event",
-    "prizes": [
-        {
-            id: 1,
-            amount: 300, 
-            name: "1st Prize",
-            description: "Some descripiton here"
-        },
-        {
-            id: 2,
-            amount: 200, 
-            name: "2nd Prize",
-            description: "Some descripiton here"
-        },
-        {
-            id: 3,
-            amount: 100, 
-            name: "2rd Prize",
-            description: "Some descripiton here"
-        }
-    ]
-  },
+  "date": "2023-04-15",
+  "time": "10:00am",
+  "name": "Spring Fair",
+  "venue": "Central Park",
+  "hosts": [
+    {
+      "name": "John Smith",
+      "email": "john.smith@example.com"
+    },
+    {
+      "name": "Jane Doe",
+      "email": "jane.doe@example.com"
+    }
+  ],
+  "sponsors": [
+    {
+      "name": "ABC Company",
+      "website": "https://www.abc.com/"
+    },
+    {
+      "name": "XYZ Inc.",
+      "website": "https://www.xyz.com/"
+    }
+  ],
+  "contacts": [
+    {
+      "name": "Joe Brown",
+      "email": "joe.brown@example.com",
+      "phone": "+1-555-1234"
+    },
+    {
+      "name": "Sara Jones",
+      "email": "sara.jones@example.com",
+      "phone": "+1-555-5678"
+    }
+  ],
+  "imagePortrait": "https://www.example.com/images/spring-fair-portrait.jpg",
+  "imageLandscape": "https://www.example.com/images/spring-fair-landscape.jpg",
+  "admission": "Free",
+  "type": "community_event",
+  "prizes": [
+    {
+      id: 1,
+      amount: 300,
+      name: "1st Prize",
+      description: "Some descripiton here"
+    },
+    {
+      id: 2,
+      amount: 200,
+      name: "2nd Prize",
+      description: "Some descripiton here"
+    },
+    {
+      id: 3,
+      amount: 100,
+      name: "2rd Prize",
+      description: "Some descripiton here"
+    }
+  ]
+},
 
-  {
-    "date": "2023-04-15",
-    "time": "10:00am",
-    "name": "Spring Fair",
-    "venue": "Central Park",
-    "hosts": [
-      {
-        "name": "John Smith",
-        "email": "john.smith@example.com"
-      },
-      {
-        "name": "Jane Doe",
-        "email": "jane.doe@example.com"
-      }
-    ],
-    "sponsors": [
-      {
-        "name": "ABC Company",
-        "website": "https://www.abc.com/"
-      },
-      {
-        "name": "XYZ Inc.",
-        "website": "https://www.xyz.com/"
-      }
-    ],
-    "contacts": [
-      {
-        "name": "Joe Brown",
-        "email": "joe.brown@example.com",
-        "phone": "+1-555-1234"
-      },
-      {
-        "name": "Sara Jones",
-        "email": "sara.jones@example.com",
-        "phone": "+1-555-5678"
-      }
-    ],
-    "imagePortrait": "https://www.example.com/images/spring-fair-portrait.jpg",
-    "imageLandscape": "https://www.example.com/images/spring-fair-landscape.jpg",
-    "admission": "$5",
-    "type": "community_event",
-    "prizes": []
-  }, 
-  {
-    "date": "2023-05-15",
-    "time": "19:00",
-    "name": "Spring Music Festival",
-    "venue": "Central Park",
-    "hosts": [
-      {
-        "name": "John Smith",
-        "email": "john.smith@example.com"
-      },
-      {
-        "name": "Jane Doe",
-        "email": "jane.doe@example.com"
-      }
-    ],
-    "sponsors": [
-      {
-        "name": "ABC Company",
-        "logo": "https://example.com/abc.png"
-      },
-      {
-        "name": "XYZ Corporation",
-        "logo": "https://example.com/xyz.png"
-      }
-    ],
-    "contacts": [
-      {
-        "name": "Bob Johnson",
-        "email": "bob.johnson@example.com",
-        "phone": "555-1234"
-      },
-      {
-        "name": "Sally Wong",
-        "email": "sally.wong@example.com",
-        "phone": "555-5678"
-      }
-    ],
-    "imagePortrait": "https://example.com/event.jpg",
-    "imageLandscape": "https://example.com/event_landscape.jpg",
-    "admission": "Free",
-    "type": "Music Festival",
-    "prizes": [
-      {
-        "name": "Grand Prize",
-        "description": "A trip to Hawaii"
-      },
-      {
-        "name": "Runner-up Prize",
-        "description": "A new guitar"
-      }
-    ]
-  }
+{
+  "date": "2023-04-15",
+  "time": "10:00am",
+  "name": "Spring Fair",
+  "venue": "Central Park",
+  "hosts": [
+    {
+      "name": "John Smith",
+      "email": "john.smith@example.com"
+    },
+    {
+      "name": "Jane Doe",
+      "email": "jane.doe@example.com"
+    }
+  ],
+  "sponsors": [
+    {
+      "name": "ABC Company",
+      "website": "https://www.abc.com/"
+    },
+    {
+      "name": "XYZ Inc.",
+      "website": "https://www.xyz.com/"
+    }
+  ],
+  "contacts": [
+    {
+      "name": "Joe Brown",
+      "email": "joe.brown@example.com",
+      "phone": "+1-555-1234"
+    },
+    {
+      "name": "Sara Jones",
+      "email": "sara.jones@example.com",
+      "phone": "+1-555-5678"
+    }
+  ],
+  "imagePortrait": "https://www.example.com/images/spring-fair-portrait.jpg",
+  "imageLandscape": "https://www.example.com/images/spring-fair-landscape.jpg",
+  "admission": "$5",
+  "type": "community_event",
+  "prizes": []
+},
+{
+  "date": "2023-05-15",
+  "time": "19:00",
+  "name": "Spring Music Festival",
+  "venue": "Central Park",
+  "hosts": [
+    {
+      "name": "John Smith",
+      "email": "john.smith@example.com"
+    },
+    {
+      "name": "Jane Doe",
+      "email": "jane.doe@example.com"
+    }
+  ],
+  "sponsors": [
+    {
+      "name": "ABC Company",
+      "logo": "https://example.com/abc.png"
+    },
+    {
+      "name": "XYZ Corporation",
+      "logo": "https://example.com/xyz.png"
+    }
+  ],
+  "contacts": [
+    {
+      "name": "Bob Johnson",
+      "email": "bob.johnson@example.com",
+      "phone": "555-1234"
+    },
+    {
+      "name": "Sally Wong",
+      "email": "sally.wong@example.com",
+      "phone": "555-5678"
+    }
+  ],
+  "imagePortrait": "https://example.com/event.jpg",
+  "imageLandscape": "https://example.com/event_landscape.jpg",
+  "admission": "Free",
+  "type": "Music Festival",
+  "prizes": [
+    {
+      "name": "Grand Prize",
+      "description": "A trip to Hawaii"
+    },
+    {
+      "name": "Runner-up Prize",
+      "description": "A new guitar"
+    }
+  ]
+},
+
+{
+  "date": "2023-07-07",
+  "time": "10:00am",
+  "name": "Afrihub Hackathon",
+  "venue": "Harare Meseum",
+  "hosts": [
+    {
+      "name": "John Smith",
+      "email": "john.smith@example.com"
+    },
+    {
+      "name": "Jane Doe",
+      "email": "jane.doe@example.com"
+    }
+  ],
+  "sponsors": [
+    {
+      "name": "Econet",
+      "website": "https://www.econet.co.zw/"
+    },
+    {
+      "name": "XYZ Inc.",
+      "website": "https://www.xyz.com/"
+    }
+  ],
+  "contacts": [
+    {
+      "name": "Joe Brown",
+      "email": "joe.brown@example.com",
+      "phone": "+1-555-1234"
+    },
+    {
+      "name": "Sara Jones",
+      "email": "sara.jones@example.com",
+      "phone": "+1-555-5678"
+    }
+  ],
+  "imagePortrait": "https://plus.unsplash.com/premium_photo-1681400688788-a5fd7e7bcd89?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=580&q=80",
+  "imageLandscape": "https://images.unsplash.com/photo-1638029202288-451a89e0d55f?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=870&q=80",
+  "admission": "Free",
+  "type": "community_event",
+  "prizes": [
+    {
+      id: 1,
+      amount: 300,
+      name: "1st Prize",
+      description: "Some descripiton here"
+    },
+    {
+      id: 2,
+      amount: 200,
+      name: "2nd Prize",
+      description: "Some descripiton here"
+    },
+    {
+      id: 3,
+      amount: 100,
+      name: "2rd Prize",
+      description: "Some descripiton here"
+    }
+  ]
+},
+
+
+{
+  "date": "2023-07-07",
+  "time": "10:00am",
+  "name": "Dev Fun Meetup",
+  "venue": "Africa Unity Square",
+  "hosts": [
+    {
+      "name": "John Smith",
+      "email": "john.smith@example.com"
+    },
+    {
+      "name": "Jane Doe",
+      "email": "jane.doe@example.com"
+    }
+  ],
+  "sponsors": [
+    {
+      "name": "Zindi",
+      "website": "https://www.econet.co.zw/"
+    },
+    {
+      "name": "Netro Zim",
+      "website": "https://www.xyz.com/"
+    }
+  ],
+  "contacts": [
+    {
+      "name": "Joe Brown",
+      "email": "joe.brown@example.com",
+      "phone": "+1-555-1234"
+    },
+    {
+      "name": "Sara Jones",
+      "email": "sara.jones@example.com",
+      "phone": "+1-555-5678"
+    }
+  ],
+  "imagePortrait": "https://images.unsplash.com/photo-1594007654729-407eedc4be65?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=428&q=80",
+  "imageLandscape": "https://images.unsplash.com/photo-1513104890138-7c749659a591?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=870&q=80",
+  "admission": "Free",
+  "type": "community_event",
+  "prizes": [
+    
+  ]
+}
 
 ]
 
 
-  module.exports = events 
+module.exports = events 
